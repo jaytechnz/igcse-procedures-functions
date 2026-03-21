2b20d52a1a1719e68b56cb8c6eb003c6304d73aa
Jenny Smith <53411671+jaytechnz@users.noreply.github.com>
2026-03-21 16:29:08 +1300

    Add files via upload
    Task 8 - pseudoKeys updated.

--- app.js
+++ app.js
@@ -779,7 +779,7 @@ const tasks=[
 
   /* Task 8 */
 {t:'Write a Function',d:'medium',
-  pseudoKeys:['FUNCTION', 'REAL', 'AddGST','Price','RETURN', 'RETURNS','1.15','230','ENDFUNCTION'],
+  pseudoKeys:['FUNCTION', 'REAL', 'AddGST','Price','RETURN', 'RETURNS','1.15','230','ENDFUNCTION', 'DECLARE', 'PriceWithTax'],
   javaKeys:['double','addGST','price','return','1.15','230'],
   b:`<p><strong>(a)</strong> Write a function, AddGST(), in <code>pseudocode</code> that takes a REAL parameter, <code>Price</code>. Your function should return the updated Price after 15% tax has been added to the parameter's value.</p>
   <p>
