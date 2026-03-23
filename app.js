3e6c998ac7c8638212b6daf23b8195f1d0eb6571
Jenny Smith <53411671+jaytechnz@users.noreply.github.com>
2026-03-23 18:31:04 +1300

    Add files via upload
    Updated Task 4 to only look pseudocode as well as Java.
    Updated the task's wording so students may choose to re-write the amended algorithm in either pseducode OR Java.

--- app.js
+++ app.js
@@ -745,9 +745,9 @@ const tasks=[
 
   /* Task 4 */
 {t:'Spot the Error',d:'easy',
-  pseudoKeys:['INTEGER','STRING','type','mismatch','15','comma','concatenation'],
-  javaKeys:['void', 'int','String','type','mismatch','15'],
-  b:`This code has <b>two errors</b>. Find and fix both:\n<pre>PROCEDURE SayAge(Age : INTEGER)\n    OUTPUT "You are " &amp; Age &amp; " years old"\nENDPROCEDURE\n\nCALL SayAge("fifteen")</pre>\n<b>(a)</b> Describe what is wrong with the argument in the CALL statement.<br><b>(b)</b> Describe what is wrong with the OUTPUT line in the procedure.<br><b>(c)</b> Rewrite the corrected procedure in <code>pseudocode</code>.
+  pseudoKeys:['INTEGER','STRING','type','mismatch','15','comma','concatenation', ','],
+  javaKeys:['void', 'int','String','type','mismatch','15', '+'],
+  b:`This code has <b>two errors</b>. Find and fix both:\n<pre>PROCEDURE SayAge(Age : INTEGER)\n    OUTPUT "You are " &amp; Age &amp; " years old"\nENDPROCEDURE\n\nCALL SayAge("fifteen")</pre>\n<b>(a)</b> Describe what is wrong with the argument in the CALL statement.<br><b>(b)</b> Describe what is wrong with the OUTPUT line in the procedure.<br><b>(c)</b> Rewrite the corrected procedure in either <code>pseudocode</code> OR <code>Java</code>.
   <div class="task-hint">
   💡 Use any error messages that may display after you check your answers to refine your answer before clicking the 'Check My Answer' button again.<p></p>
   </div>`},
